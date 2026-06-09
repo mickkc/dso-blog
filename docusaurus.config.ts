680c12e9a3d9d245da562708e7a3fa978a0e0f91
Mickkc <tim@ndu.ad>
2026-06-09 15:48:55 +0200

    Added projects link to footer, removed community section

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -100,22 +100,9 @@ const config: Config = {
               label: 'Tutorial',
               to: '/docs/guides/intro',
             },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
+              label: 'Projects',
+              to: '/docs/projects',
             },
           ],
         },
