e15b700195915444eb6794303bf280ccbb858c59
Mickkc <tim@ndu.ad>
2026-06-09 15:26:12 +0200

    Updated title, tagline and icon

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,8 +9,8 @@ dotenvconfig();
 const blogEnabled = Boolean(process.env.BLOG_ENABLED === 'true')
 
 const config: Config = {
-  title: 'DSO Live Demo Docs',
-  tagline: 'Dinosaurs are cool',
+  title: 'Tim\'s DevSecOps Blog',
+  tagline: 'DevSecOps Enthusiast with a passion for details and efficiency',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -71,10 +71,10 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'Tim\'s DevSecOps Blog',
       logo: {
         alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        src: 'img/logo.png',
       },
       items: [
         {
